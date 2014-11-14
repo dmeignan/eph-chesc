58f03d56ba33c16fe0d4015b950f5a353ed1bd0c
dmeignan <dmeignan@vidar.informatik.uni-osnabrueck.de>
2014-11-14 11:30:20 +0100

    Starting package.

---
+++ EphChesc/src/fr/lalea/eph/EphCommand.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2013-2014, Universitaet Osnabrueck
+ * Author: David Meignan
+ */
+package fr.lalea.eph;
+
+/**
+ * The <code>EphCommand</code> class allows to run EPH on the 
+ * CHeSC benchmark using command line parameters.
+ * 
+ * @author David Meignan
+ */
+public class EphCommand {
+
+	/**
+	 * Runs EPH on the CHeSC benchmark.
+	 * 
+	 * @param args arguments that are managed by the
+	 * <code>SolverParameters</code> class.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ EphChesc/src/fr/lalea/eph/RunParameters.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2013-2014, Universitaet Osnabrueck
+ * Author: David Meignan
+ */
+package fr.lalea.eph;
+
+/**
+ * Command line parameters for running EPH on instances of the
+ * CHeSC benchmark.
+ * 
+ * @author David Meignan
+ */
+public class RunParameters {
+
+}
